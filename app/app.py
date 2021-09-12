1c069003222d3c67104133ee08be61378232a114
sasaco <sasaicco@gmail.com>
2021-09-12 22:48:14 +0900

    are you lady??

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
 import json
-import sys
+import gmsh
 
 def handler(event, context):
-
-    sys.path.append('/usr/local/lib/python3.8/site-packages/gmsh-4.8.4-Linux64-sdk/lib')
 
     return {
         "statusCode": 200,
