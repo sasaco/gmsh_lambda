015f7c47453c6a57adc5cfb02836309ed9ccaa43
sasaco <sasaicco@gmail.com>
2021-09-22 11:52:28 +0900

    わからん "message": "Internal server error"

--- app/app.py
+++ app/app.py
@@ -32,6 +32,7 @@ def handler(event, context):
     return {
         "statusCode": 200,
         "headers": {
+            "Access-Control-Allow-Origin":"*",
             "Content-Type": "application/json"
         },
         "body": json.dumps(
